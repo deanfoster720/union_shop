297543b855868a27b895faca20ca87dc102bec91
Dean Foster <deanfoster720@gmail.com>
2025-11-26 16:44:48 +0000

    Change placeholder header text

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -22,7 +22,7 @@ class Header extends StatelessWidget {
             padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
             color: const Color(0xFF4d2963),
             child: const Text(
-              'PLACEHOLDER HEADER TEXT',
+              'BIG SALE! OUR ESSENTIAL RANGE HAS DROPPED IN PRICE! OVER 20% OFF! COME GRAB YOURS WHILE STOCK LASTS!',
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.white, fontSize: 16),
             ),
