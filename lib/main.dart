d814bec0b1ecf1e7f97446319bf03110f79605a1
Dean Foster <deanfoster720@gmail.com>
2025-11-26 17:58:19 +0000

    Remove static route

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,6 @@ class UnionShopApp extends StatelessWidget {
       home: const HomeScreen(),
       // By default, the app starts at the '/' route, which is the HomeScreen
       initialRoute: '/',
-      // When navigating to '/product', build and return the ProductPage
-      // In your browser, try this link: http://localhost:49856/#/product
-      routes: {'/product': (context) => const ProductPage()},
     );
   }
 }
