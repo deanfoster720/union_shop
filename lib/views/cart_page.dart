049ee7a524d6ccc259708117dab78c84dc918e8e
Dean Foster <deanfoster720@gmail.com>
2025-11-26 18:31:10 +0000

    Wire CartPage into BaseScaffold layout

--- lib/views/cart_page.dart
+++ lib/views/cart_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/widgets/base_scaffold.dart';
+import 'package:union_shop/widgets/header.dart';
+import 'package:union_shop/widgets/footer.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -8,15 +11,27 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  void navigateToHome(BuildContext context) {
+    Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+  }
+
+  void placeholderCallbackForButtons() {}
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cart'),
+    return BaseScaffold(
+      header: Header(
+        onLogoTap: () => navigateToHome(context),
+        onPlaceholderPressed: placeholderCallbackForButtons,
       ),
-      body: const Center(
-        child: Text('Cart page'),
+      body: const Padding(
+        padding: EdgeInsets.all(24.0),
+        child: Text(
+          'Your cart',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
       ),
+      footer: const Footer(),
     );
   }
 }
