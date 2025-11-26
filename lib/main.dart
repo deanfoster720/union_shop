b35997c5f2858d9cef7cdabe77108ed690c34d4c
Dean Foster <deanfoster720@gmail.com>
2025-11-26 17:59:27 +0000

    Remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/views/home_page.dart';
-import 'package:union_shop/views/product_page.dart';
 
 void main() {
   runApp(const UnionShopApp());
