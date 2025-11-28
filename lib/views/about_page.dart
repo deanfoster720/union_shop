38e58a6127847e56a5497fcd4b2b7ed63eb22813
Dean Foster <deanfoster720@gmail.com>
2025-11-28 10:15:10 +0000

    Update about_page.dart text

--- lib/views/about_page.dart
+++ lib/views/about_page.dart
@@ -23,11 +23,33 @@ class AboutPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: const [
             Text(
-              'About Union Shop',
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
+              'Welcome to the Union Shop!',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'We’re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!',
+              style: TextStyle(fontSize: 16, color: Colors.grey, height: 1.5),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'All online purchases are available for delivery or instore collection!',
+              style: TextStyle(fontSize: 16, color: Colors.grey, height: 1.5),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don’t hesitate to contact us at hello@upsu.net.',
+              style: TextStyle(fontSize: 16, color: Colors.grey, height: 1.5),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Happy shopping!',
+              style: TextStyle(fontSize: 16, color: Colors.grey, height: 1.5),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'The Union Shop & Reception Team',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
             ),
           ],
         ),
